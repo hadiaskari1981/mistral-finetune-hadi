4ada6221a15152091b73bf495d84c7d9ccd88cd2
Hadi Askari <hadi.askari@nextory.com>
2024-06-07 20:10:35 +0200

    testing the code

---
+++ data_preprocess.py
@@ -0,0 +1,98 @@
+import argparse
+import json
+import logging
+import os
+import time
+from pathlib import Path
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+
+def save_dataset(dataset, data_path, tag='train'):
+
+    project_name = 'mistral'
+
+    metadata = {
+        'dataset-versions': [
+            {
+                'uri': f'dataset://viggo/{project_name}_{tag}',
+                'targeting_aliases': [f'dev_{tag}'],
+                'tags': ['dev', 'mistral'],
+            },
+        ],
+    }
+    out = f'encoded_{tag}'
+    save_path = os.path.join(data_path, out)
+    dataset.save_to_disk(save_path)
+
+    for file in os.listdir(save_path):
+        with open(os.path.join(data_path, f'{file}.metadata.json'), 'w') as outfile:
+            json.dump(metadata, outfile)
+
+
+class DataPreprocessor:
+    def __init__(self, args):
+        self.data_path = args["data_path"]
+        self.model_max_length = args["model_max_length"]
+        self.tokenizer = args["tokenizer"]
+        self.force_save = args["force_save"]
+        dataset = load_dataset(
+            'csv',
+            data_files={
+                'train': os.path.join(self.data_path, 'train.csv'),
+                'val': os.path.join(self.data_path, 'validation.csv'),
+                'test': os.path.join(self.data_path, 'test.csv'),
+            },
+        )
+        self.train = dataset['train']
+        self.eval = dataset['val']
+        self.test = dataset['test']
+
+    def generate_and_tokenize_prompt(self, data_point, tokenizer):
+        full_prompt = f"""Given a meaning representation generate a target sentence that utilizes the attributes and 
+        attribute values given. The sentence should use all the information provided in the meaning representation. 
+        ### Target sentence: {data_point["ref"]}
+
+        ### Meaning representation:
+        {data_point["mr"]}
+        """
+        return tokenizer(full_prompt, truncation=True, max_length=self.model_max_length, padding='max_length')
+
+    def load_and_prepare_and_save_data(self):
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.tokenizer,
+            model_max_length=self.model_max_length,
+            padding_side='left',
+            add_eos_token=True,
+        )
+        tokenizer.pad_token = tokenizer.eos_token
+
+        tokenized_train = self.train.map(self.generate_and_tokenize_prompt)
+        tokenized_val = self.eval.map(self.generate_and_tokenize_prompt)
+
+        if self.force_save:
+            save_dataset(tokenized_train, self.data_path, 'train')
+            save_dataset(tokenized_val,  self.data_path, 'val')
+            save_dataset(self.test, self.data_path, 'test')
+        else:
+            return tokenized_train, tokenized_val, self.test
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # Add arguments based on your script's needs
+    args = {
+        "data_path": "",
+        "tokenizer": 'mistralai/Mistral-7B-v0.1',
+        "model_max_length": 512,
+        "force_save": True
+    }
+
+    data_preprocessor = DataPreprocessor(*args)
+
+    data_preprocessor.load_and_prepare_and_save_data()
+
+
+if __name__ == '__main__':
+    main()
---
+++ inference-mistral.py
@@ -0,0 +1,71 @@
+import argparse
+import logging
+import torch
+import transformers
+from peft import PeftModel
+from helpers import get_quantization_config
+
+logger = logging.getLogger(__name__)
+
+
+class ModelInference:
+    def __init__(self, model_path: str, checkpoint_path: str) -> None:
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            model_path,
+            model_max_length=512,
+            padding_side='left',
+            add_eos_token=True,
+        )
+        logger.info('Loading model...')
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_path,
+            quantization_config=get_quantization_config(),
+        )
+        logger.info('Creating PEFT model...')
+        self.ft_model = PeftModel.from_pretrained(model, checkpoint_path).eval()
+
+    def generate_response(self, prompt: str, max_tokens: int = 50) -> str:
+        inputs = self.prepare_prompt(prompt)
+        with torch.no_grad():
+            logger.info('Generating up to %d tokens...', max_tokens)
+            outputs = self.ft_model.generate(**inputs, max_length=max_tokens, pad_token_id=2)
+        logger.info('Decoding...')
+        return self.postprocess(self.tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+    def prepare_prompt(self, prompt):
+        return self.tokenizer(prompt, return_tensors='pt')
+
+    def postprocess(self, original_string):
+        return original_string.strip()
+
+
+def run(args):
+    inference = ModelInference(
+        model_path=args.base_mistral_model,
+        checkpoint_path=args.checkpoint_path,
+    )
+    response = inference.generate_response(
+        prompt=args.prompt,
+        max_tokens=args.max_tokens,
+    )
+    print('Generated Response:')
+    print(response)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Fine-tuned Model Inference')
+    # fmt: off
+    parser.add_argument('--base_mistral_model', type=str, default='mistralai/Mistral-7B-v0.1', help='Base mistral from hugging face')
+    parser.add_argument('--checkpoint_path', default='/valohai/inputs/finetuned-checkpoint/')
+    parser.add_argument('--max_tokens', type=int, default=305, help='Maximum number of tokens in response')
+    parser.add_argument('--model_path', default='/valohai/inputs/model-base/')
+    parser.add_argument('--prompt', type=str, required=True, help='Input prompt for text generation')
+    # fmt: on
+
+    args = parser.parse_args()
+    run(args)
+
+
+if __name__ == '__main__':
+    main()
