54029c31e2c710dfe14e0d3ee2f18e6108956ea0
Hadi Askari <hadi.askari@nextory.com>
2024-06-09 16:02:34 +0200

    create git ignore to avoid push data to git

--- data.py
+++ data.py
@@ -192,6 +192,7 @@ def inference(args):
     base_model_name = args.get("base_model_name")
     cache_dir = args.get("cache_dir")
     output_dir = args.get("output_dir")
+    target_sentence = args.get("target_sentence")
     checkpoint_version = args.get("checkpoint")
     eval_tokenizer = AutoTokenizer.from_pretrained(
         base_model_name,
@@ -201,12 +202,12 @@ def inference(args):
     )
 
     ft_model = PeftModel.from_pretrained(base_model, f"{output_dir}/{checkpoint_version}")
-    eval_prompt = """Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+    eval_prompt = f"""Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
     This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
     The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
 
     ### Target sentence:
-    Earlier, you stated that you didn't have strong feelings about PlayStation's Little Big Adventure. Is your opinion true for all games which don't have multiplayer?
+    {target_sentence}
 
     ### Meaning representation:
     """
